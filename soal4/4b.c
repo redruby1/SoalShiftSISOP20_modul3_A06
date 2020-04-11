47b30974cf39ff7c6ffbdf150e25ee5324c093f4
Anisa A <56338326+redruby1@users.noreply.github.com>
2020-04-11 19:48:09 +0700

    update 4b

--- soal4/4b.c
+++ soal4/4b.c
@@ -7,66 +7,63 @@
 #include <sys/wait.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <time.h>
 
-pthread_t tid[5*2];
-int count = 0;
-int i, j, k, step = 0, hasil;
+pthread_t tid[20];
+int i, j, k, hasil;
 
-int C[4][5], D[4][5];
+int E[5][5], D[5][5];
 
 int jumlah(int a) {
-	hasil = hasil + jumlah(a-1);
-	return hasil;
+	if(a == 0) return 0; 
+    return (a+jumlah(a - 1));
 }
 
-void sum(void *arg) {
-	int core = count++;
-	
-//	for(i=0; i<4; i++) {
-	
-	for(i=(core*5)/5; i<((core+1)*5)/5; i++) {
-		for (j=0; j<5; j++) {
-			D[i][j] = jumlah(C[i][j]);
+void *sum(void *arg) {	
+	for(i=0; i<4; i++) {
+		for(j=0; j<5; j++) {
+			D[i][j] = jumlah(E[i][j]);
 		}
 	}
+	
+	pthread_exit(NULL);
 }
 
 int main() {
-	key_t key = 5678;
+	key_t key = 1234;
+	int *angka;
 	
-	int C[4][5], D[4][5];
+	int shmid = shmget(key, sizeof(int)*5*5, IPC_CREAT | 0666);
+	angka = shmat(shmid, NULL, 0);
 	
-	int shmid = shmget(key, sizeof(int)*5*4, IPC_CREAT | 0666);
-	C[4][5] = shmat(shmid, NULL, 0);
-	
-	int i, j;
 	for(i=0; i<4; i++) {
 		for(j=0; j<5; j++) {
-			printf("%d\t",C[i][j]);
+			E[i][j] = angka[i*5 + j];
+			printf("%d\t",E[i][j]);
+			
+			sleep(1);
 		}
 		printf("\n");
 	}
 	
-	for(i=0; i<5; i++) { 
-		int *p; 
-		pthread_create(&tid[i], NULL, sum, (void*)(p));
-//		step++;
+	sleep(10);
+	
+	for(i=0; i<20; i++) {
+		pthread_create(&tid[i], NULL, sum, (void*)&i);
     }
     
-    for(i=0; i<5*2; i++) {
+    for(i=0; i<20; i++) {
 		pthread_join(tid[i], NULL);
 	}
-
-//	sum(C);
     
     printf("++++++++\n");
     for(i=0; i<4; i++) { 
         for(j=0; j<5; j++) {
-        	printf("%d ", D[i][j]);
+        	printf("%d\t", D[i][j]);
 		}
         printf("\n");
     }
 	
-	shmdt(C);
+	shmdt(angka);
     shmctl(shmid, IPC_RMID, NULL);
 }
